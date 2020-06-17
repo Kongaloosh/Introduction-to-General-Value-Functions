b34056c5a0cbe452f011d0d8609374c8b2dd2db2
kongaloosh <hi@kongaloosh.com>
2020-06-16 21:03:22 -0600

    Adding a modified stateless version of Rory's Bento arm simulator: simply oscillates a joint back and forth.

---
+++ pysrc/bento_simulator.py
@@ -0,0 +1,30 @@
+
+def get_state(steps, normalized_position, normalized_load, normalized_velocity):
+    """Takes the current state of the simulated bento arm and updates it."""
+    sim_velocity = 0.01  # simulation velocity
+    steps += 1
+
+    # Simulate the shoulder moving back and forth between positions 0 and 1
+    # we change the velocity between postive and negative values depending on the position of the joint
+    if normalized_position <= 0:
+        normalized_velocity = sim_velocity
+    elif normalized_position >= 1:
+        normalized_velocity = -sim_velocity
+
+    normalized_position += normalized_velocity
+
+    if normalized_position >= 0.5:
+        is_moving = 1
+    else:
+        is_moving = 0
+
+    return steps, normalized_position, normalized_load, normalized_velocity
+
+    # state_joints = [normalized_position * 50,
+    #                 (normalized_velocity * 100 + 1) / 2 * 10]
+
+    # prompt_msg = 'Step Number: ' + str(number_of_steps) + ' Shoulder Pos: ' + str(
+    #     "%.3f" % np.absolute(round(normalized_position, 3))) + ' Gripper Movement: ' + str(
+    #     is_moving) + ' Gripper Prediction: ' + str("%.5f" % np.nan) + ' Gripper Return: ' + str(
+    #     np.nan)  # print steps elapsed since beginning of experiment
+
